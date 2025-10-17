6c8a3bad7453b70f0ede4ab4e42a1cdc623c7f98
Emerson Willian <emersonwdias2013@gmail.com>
2025-10-17 17:16:25 -0300

    Boas Vindas

--- application/AplicacaoBiblioteca.java
+++ application/AplicacaoBiblioteca.java
@@ -2,6 +2,6 @@ package application;
 
 public class AplicacaoBiblioteca {
    public static void main(String[] args) {
-
+	  System.out.println("Seja-Bem Vindo ao nosso Sistema Biblioteca");
    }
 }
