180d33831ed4cc4261b154cd00f952be085c044b
Gustavo Henrique <gh33493@gmail.com>
2025-10-19 12:59:19 -0300

    opcoes
    opcoes de escolha no main

--- application/AplicacaoBiblioteca.java
+++ application/AplicacaoBiblioteca.java
@@ -1,7 +1,39 @@
 package application;
+import java.util.Scanner;
 
 public class AplicacaoBiblioteca {
-   public static void main(String[] args) {
-	  System.out.println("Seja-Bem Vindo ao nosso Sistema Biblioteca");
+   public static void main(String[] args){
+      int escolha;
+      Scanner entrada= new Scanner(System.in);
+      
+      do{
+         System.out.println("---OPCOES---\n");
+         System.out.println("1- Cadastrar.");
+         System.out.println("2- Login");
+         System.out.println("0- Encerrar");
+         System.out.printf("Qual opcao deseja: ");
+         escolha=entrada.nextInt();
+
+         switch(escolha){
+         case 1:
+            System.out.println("Cadastrando.\n");
+            break;
+
+         case 2:
+            System.out.println("Faxendo Login.\n");
+            break;
+
+         case 0:
+            System.out.println("Encerrando programa.\n");
+            entrada.close();
+            break;
+
+         default:
+            System.out.println("Opção invalida.\n");
+            break;
+         }
+      }while(escolha!=0);
+
    }
+   
 }
