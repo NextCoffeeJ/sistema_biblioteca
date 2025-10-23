a089feca23b70252c4b34c6254444cf4c83da01b
Gustavo Henrique <gh33493@gmail.com>
2025-10-22 22:37:59 -0300

    Atualização aluno

--- model/Aluno.java
+++ model/Aluno.java
@@ -5,23 +5,27 @@ public class Aluno extends Usuario {
    private int limiteLivro = 3;
 
    public Aluno(String nome, String cpf, String email, String telefone, String matricula, String senha, String curso) {
-      super(nome,cpf,email,telefone,matricula,senha);
-      this.curso = curso;
+	  super(nome, cpf, email, telefone, matricula, senha);
+	  this.curso = curso;
    }
-
+   
    public String getCurso() {
-   return curso;
+	  return curso;
    }
 
    public int getLimiteLivro() {
-   return limiteLivro;
+	  return limiteLivro;
    }
 
    public void setCurso(String curso) {
-   this.curso = curso;
+	  this.curso = curso;
    }
 
    public void setLimiteLivro(int limiteLivro) {
-   this.limiteLivro = limiteLivro;
+	  this.limiteLivro = limiteLivro;
+   }
+
+   public String getEmail(){
+      return email;
    }
 }
