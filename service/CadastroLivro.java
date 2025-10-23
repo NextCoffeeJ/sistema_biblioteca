4eda2aac8d14e2013919fcb877e7e2206310df21
Emerson Willian <emersonwdias2013@gmail.com>
2025-10-23 11:33:27 -0300

    classe nova

---
+++ service/CadastroLivro.java
@@ -0,0 +1,13 @@
+package service;
+
+import java.util.ArrayList;
+import model.Livro;
+
+public final class CadastroLivro {
+
+   private CadastroLivro(){}
+
+   public static void adicionarLivro(ArrayList<Livro> listaLivros, Livro livro){
+	  listaLivros.add(livro);
+   }
+}
