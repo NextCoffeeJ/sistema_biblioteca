e4bf235773a5bbfb08df79c54eea786ee1022aad
Gustavo Henrique <gh33493@gmail.com>
2025-10-22 22:42:02 -0300

    Atualização repositoriob

--- repository/BibliotecaRepository.java
+++ repository/BibliotecaRepository.java
@@ -1,4 +1,5 @@
 package repository;
 
 public class BibliotecaRepository {
+    //Eu realmente acho que isso vai ser completamente inutil então recomendo que apagem pelo menos para a entrega nesta sexta.  Se por algum motivo desconhacido a gente decidir usar essas classe a gente cria elas de novo.
 }
