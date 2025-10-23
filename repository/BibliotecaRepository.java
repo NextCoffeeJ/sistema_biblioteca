124cefe2d1be71aaba800a8699cc095de81c6bfb
Emerson Willian <emersonwdias2013@gmail.com>
2025-10-23 11:32:51 -0300

    removido

--- repository/BibliotecaRepository.java
+++
@@ -1,5 +0,0 @@
-package repository;
-
-public class BibliotecaRepository {
-    //Eu realmente acho que isso vai ser completamente inutil então recomendo que apagem pelo menos para a entrega nesta sexta.  Se por algum motivo desconhacido a gente decidir usar essas classe a gente cria elas de novo.
-}
