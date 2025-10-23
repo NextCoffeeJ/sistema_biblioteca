9a20ce84370b50f8613a795b563a4f5fc4fb95d4
Gustavo Henrique <gh33493@gmail.com>
2025-10-22 22:41:02 -0300

    Atualização Usuario

--- model/Usuario.java
+++ model/Usuario.java
@@ -1,31 +1,68 @@
 package model;
+//Penso que essa clase poderia quem saber ser abstrata ja que ta servindo só para outras classes herdarem dela, mas seila não exatamente como funciona isso.
+public class Usuario {
+   protected String nome;
+   protected String cpf;
+   protected String email;
+   protected String telefone;
+   protected String matricula;
+   protected String senha;
+   //protected double multa; Quem sabe adicionar isso aqui. Explicação no main.
+   
+   public Usuario(String nome, String cpf, String email, String telefone, String matricula, String senha) {
+	  this.nome = nome;
+	  this.cpf = cpf;
+	  this.email = email;
+	  this.telefone = telefone;
+	  this.matricula = matricula;
+	  this.senha = senha;
+   }
 
-import java.io.Serializable;
+   public String getNome() {
+	  return nome;
+   }
 
-public class Usuario implements Serializable {
-protected String nome;
-protected String cpf;
-protected String email;
-protected String telefone;
-protected String matricula;
-protected String senha;
+   public String getCpf() {
+	  return cpf;
+   }
 
-public Usuario(String nome, String cpf, String email, String telefone, String matricula, String senha) {
-	this.nome = nome;
-	this.cpf = cpf;
-	this.email = email;
-	this.telefone = telefone;
-	this.matricula = matricula;
-	this.senha = senha;
-}
+   public String getEmail() {
+	  return email;
+   }
 
-public void exibirDados() {
-	System.out.println("Nome: " + nome);
-	System.out.println("CPF: " + cpf);
-	System.out.println("Email: " + email);
-	System.out.println("Telefone: " + telefone);
-	System.out.println("Matrícula: " + matricula);
-	System.out.println("Senha: " + senha);
-	System.out.println("----------------------");
-}
+   public String getTelefone() {
+	  return telefone;
+   }
+
+   public String getMatricula() {
+	  return matricula;
+   }
+
+   public String getSenha() {
+	  return senha;
+   }
+
+   public void setNome(String nome) {
+	  this.nome = nome;
+   }
+
+   public void setCpf(String cpf) {
+	  this.cpf = cpf;
+   }
+
+   public void setEmail(String email) {
+	  this.email = email;
+   }
+
+   public void setTelefone(String telefone) {
+	  this.telefone = telefone;
+   }
+
+   public void setMatricula(String matricula) {
+	  this.matricula = matricula;
+   }
+
+   public void setSenha(String senha) {
+	  this.senha = senha;
+   }
 }
