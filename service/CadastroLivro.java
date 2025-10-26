1f02fa8806097736a5167ffe897b05d7a0c626ac
Marcos Dalyson <marcosdalyson@gmail.com>
2025-10-26 18:16:57 -0300

    Atualizações

--- service/CadastroLivro.java
+++ service/CadastroLivro.java
@@ -5,19 +5,9 @@ import model.Livro;
 
 public final class CadastroLivro {
 
-   private CadastroLivro(){}
+    private CadastroLivro(){}
 
-   public static void adicionarLivro (ArrayList<Livro> listaLivros, Livro livro){
-	  listaLivros.add(livro);
-   }
-
-   public static void mostrarLivrosDisponiveis(ArrayList<Livro> listaLivros){
-       if(listaLivros.isEmpty()){
-           System.out.println("Nenhum livro cadastrado. ");
-           return;
-       }
-       for(Livro livro : listaLivros){
-           System.out.println("- " +livro.getTitulo());
-       }
-   }
-
+    public static void adicionarLivro (ArrayList<Livro> listaLivros, Livro livro){
+        listaLivros.add(livro);
+    }
+}
