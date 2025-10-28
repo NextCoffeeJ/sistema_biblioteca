8fbddcc9f1dfba1278e731049cf3cc5b847aa4bc
MarcelaCantalice <marcelacantalice0@gmail.com>
2025-10-28 09:51:49 -0300

    Update Usuario.java

--- model/Usuario.java
+++ model/Usuario.java
@@ -1,76 +1,56 @@
 package model;
 
 public class Usuario {
-   protected String nome;
-   protected String cpf;
-   protected String email;
-   protected String telefone;
-   protected String matricula;
-   protected String senha;
-   protected double multa;
+    
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String matricula;
+    private String senha;
+    private double multa;
 
-   public Usuario(String nome, String cpf, String email, String telefone, String matricula, String senha) {
-	  this.nome = nome;
-	  this.cpf = cpf;
-	  this.email = email;
-	  this.telefone = telefone;
-	  this.matricula = matricula;
-	  this.senha = senha;
-   }
+    public Usuario(String nome, String cpf, String email, String telefone, String matricula, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.matricula = matricula;
+        this.senha = senha;
+        this.multa = 0.0; 
+    }
 
-   public String getNome() {
-	  return nome;
-   }
+   
+    public String getNome() { return nome; }
+    public String getCpf() { return cpf; }
+    public String getEmail() { return email; }
+    public String getTelefone() { return telefone; }
+    public String getMatricula() { return matricula; }
+    public String getSenha() { return senha; }
+    public double getMulta() { return multa; }
 
-   public String getCpf() {
-	  return cpf;
-   }
+    public void setNome(String nome) { this.nome = nome; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public void setEmail(String email) { this.email = email; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
 
-   public String getEmail() {
-	  return email;
-   }
+    
+    public void setSenha(String senha) {
+        if (senha != null && senha.length() >= 4) {
+            this.senha = senha;
+        }
+    }
 
-   public String getTelefone() {
-	  return telefone;
-   }
+    
+    public void setMulta(double multa) {
+        if (multa >= 0) {
+            this.multa = multa;
+        }
+    }
 
-   public String getMatricula() {
-	  return matricula;
-   }
-
-   public String getSenha() {
-	  return senha;
-   }
-
-   public double getMulta() {
-	  return multa;
-   }
-
-   public void setNome(String nome) {
-	  this.nome = nome;
-   }
-
-   public void setCpf(String cpf) {
-	  this.cpf = cpf;
-   }
-
-   public void setEmail(String email) {
-	  this.email = email;
-   }
-
-   public void setTelefone(String telefone) {
-	  this.telefone = telefone;
-   }
-
-   public void setMatricula(String matricula) {
-	  this.matricula = matricula;
-   }
-
-   public void setSenha(String senha) {
-	  this.senha = senha;
-   }
-
-   public void setMulta(double multa) {
-	  this.multa = multa;
-   }
+    @Override
+    public String toString() {
+        return "Usuário: " + nome + " | Matrícula: " + matricula + " | Multa: R$" + String.format("%.2f", multa);
+    }
 }
