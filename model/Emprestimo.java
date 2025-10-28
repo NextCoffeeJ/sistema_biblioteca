e0d5b2da7c8347fb2df9ed6942f4fce51af53c5b
Gustavo Henrique <gh33493@gmail.com>
2025-10-28 18:58:10 -0300

    Atualização Emprestimo
    basicamente adição de getters setters e contrutores e atualização dos memos

--- model/Emprestimo.java
+++ model/Emprestimo.java
@@ -3,14 +3,32 @@ package model;
 import java.time.LocalDate;
 
 public class Emprestimo {
-   private Usuario usuario;
+   private Aluno aluno;
+   private Professor professor;
+   private Bibliotecario bibliotecario;
    private Livro livro;
    private LocalDate dataEmprestimo;
    private LocalDate dataLimiteDevolucao;
    private LocalDate dataDevolucao;
 
-   public Emprestimo(Usuario usuario, Livro livro, LocalDate dataEmprestimo, LocalDate dataLimiteDevolucao) {
-	  this.usuario = usuario;
+   public Emprestimo(Aluno aluno, Livro livro, LocalDate dataEmprestimo, LocalDate dataLimiteDevolucao) {
+	  this.aluno = aluno;
+	  this.livro = livro;
+	  this.dataEmprestimo = dataEmprestimo;
+	  this.dataLimiteDevolucao = dataLimiteDevolucao;
+	  this.dataDevolucao = null;
+   }
+
+   public Emprestimo(Professor professor, Livro livro, LocalDate dataEmprestimo, LocalDate dataLimiteDevolucao) {
+	  this.professor = professor;
+	  this.livro = livro;
+	  this.dataEmprestimo = dataEmprestimo;
+	  this.dataLimiteDevolucao = dataLimiteDevolucao;
+	  this.dataDevolucao = null;
+   }
+
+   public Emprestimo(Bibliotecario bibliotecario, Livro livro, LocalDate dataEmprestimo, LocalDate dataLimiteDevolucao) {
+	  this.bibliotecario = bibliotecario;
 	  this.livro = livro;
 	  this.dataEmprestimo = dataEmprestimo;
 	  this.dataLimiteDevolucao = dataLimiteDevolucao;
@@ -18,13 +36,17 @@ public class Emprestimo {
    }
 
 
-   public Usuario getUsuario() { return usuario; }
+   public Aluno getAluno() { return aluno; }
+   public Professor getProfessor() { return professor;}
+   public Bibliotecario getBibliotecario(){return bibliotecario;}
    public Livro getLivro() { return livro; }
    public LocalDate getDataEmprestimo() { return dataEmprestimo; }
    public LocalDate getDataLimiteDevolucao() { return dataLimiteDevolucao; }
    public LocalDate getDataDevolucao() { return dataDevolucao; }
 
-   public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+   public void setAluno(Aluno aluno) { this.aluno = aluno; }
+   public void setProfessor(Professor professor){this.professor=professor;}
+   public void setBibliotecario(Bibliotecario bibliotecario){this.bibliotecario=bibliotecario;}
    public void setLivro(Livro livro) { this.livro = livro; }
    public void setDataEmprestimo(LocalDate dataEmprestimo) { this.dataEmprestimo = dataEmprestimo; }
    public void setDataLimiteDevolucao(LocalDate dataLimiteDevolucao) { this.dataLimiteDevolucao = dataLimiteDevolucao; }
