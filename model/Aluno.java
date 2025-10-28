37a3b0cafa845141895d31dd31c8f3c4f7c6e44b
MarcelaCantalice <marcelacantalice0@gmail.com>
2025-10-28 09:38:24 -0300

    Update Aluno.java

--- model/Aluno.java
+++ model/Aluno.java
@@ -1,31 +1,33 @@
 package model;
 
 public class Aluno extends Usuario {
-   private String curso;
-   private int limiteLivro = 3;
+    private String curso;
+    private int limiteLivro;
 
-   public Aluno(String nome, String cpf, String email, String telefone, String matricula, String senha, String curso) {
-	  super(nome, cpf, email, telefone, matricula, senha);
-	  this.curso = curso;
-   }
+    public Aluno(String nome, String cpf, String email, String telefone, String matricula, String senha, String curso) {
+        super(nome, cpf, email, telefone, matricula, senha);
+        this.curso = curso;
+        this.limiteLivro = 3; 
+    }
 
-   public String getCurso() {
-	  return curso;
-   }
+    public String getCurso() {
+        return curso;
+    }
 
-   public int getLimiteLivro() {
-	  return limiteLivro;
-   }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
 
-   public void setCurso(String curso) {
-	  this.curso = curso;
-   }
+    public int getLimiteLivro() {
+        return limiteLivro;
+    }
 
-   public void setLimiteLivro(int limiteLivro) {
-	  this.limiteLivro = limiteLivro;
-   }
+    public void setLimiteLivro(int limiteLivro) {
+        this.limiteLivro = limiteLivro;
+    }
 
-   public String getEmail(){
-	  return email;
-   }
+    @Override
+    public String toString() {
+        return "Aluno: " + getNome() + " | Curso: " + curso + " | Limite de livros: " + limiteLivro;
+    }
 }
