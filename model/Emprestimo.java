0d825082ef325918c61d26aeff6de1aac4f5e4cd
MarcelaCantalice <marcelacantalice0@gmail.com>
2025-10-29 17:46:19 -0300

    Update Emprestimo.java

--- model/Emprestimo.java
+++ model/Emprestimo.java
@@ -1,63 +1,100 @@
 package model;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Emprestimo {
-   private Aluno aluno;
-   private Professor professor;
-   private Bibliotecario bibliotecario;
-   private Livro livro;
-   private LocalDate dataEmprestimo;
-   private LocalDate dataLimiteDevolucao;
-   private LocalDate dataDevolucao;
+    private Usuario usuario;
+    private Livro livro;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataLimiteDevolucao;
+    private LocalDate dataDevolucao; 
+    private int qtdRenovacoes;
+    
+    private static final int LIMITE_RENOVACOES = 1;
+    private static final double MULTA_POR_DIA = 5.0; 
 
-   public Emprestimo(Aluno aluno, Livro livro, LocalDate dataEmprestimo, LocalDate dataLimiteDevolucao) {
-	  this.aluno = aluno;
-	  this.livro = livro;
-	  this.dataEmprestimo = dataEmprestimo;
-	  this.dataLimiteDevolucao = dataLimiteDevolucao;
-	  this.dataDevolucao = null;
-   }
+    public Emprestimo(Usuario usuario, Livro livro, LocalDate dataEmprestimo, LocalDate dataLimiteDevolucao) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataLimiteDevolucao = dataLimiteDevolucao;
+        this.dataDevolucao = null; 
+        this.qtdRenovacoes = 0;
+    }
 
-   public Emprestimo(Professor professor, Livro livro, LocalDate dataEmprestimo, LocalDate dataLimiteDevolucao) {
-	  this.professor = professor;
-	  this.livro = livro;
-	  this.dataEmprestimo = dataEmprestimo;
-	  this.dataLimiteDevolucao = dataLimiteDevolucao;
-	  this.dataDevolucao = null;
-   }
+    
+    public Usuario getUsuario() { return usuario; }
+    public Livro getLivro() { return livro; }
+    public LocalDate getDataEmprestimo() { return dataEmprestimo; }
+    public LocalDate getDataLimiteDevolucao() { return dataLimiteDevolucao; }
+    public LocalDate getDataDevolucao() { return dataDevolucao; }
+    public int getQtdRenovacoes() { return qtdRenovacoes; }
 
-   public Emprestimo(Bibliotecario bibliotecario, Livro livro, LocalDate dataEmprestimo, LocalDate dataLimiteDevolucao) {
-	  this.bibliotecario = bibliotecario;
-	  this.livro = livro;
-	  this.dataEmprestimo = dataEmprestimo;
-	  this.dataLimiteDevolucao = dataLimiteDevolucao;
-	  this.dataDevolucao = null;
-   }
+    
+    public void registrarDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+        this.qtdRenovacoes = 0; 
+    }
 
+    
+    public boolean isAtrasado() {
+        if (dataDevolucao == null) { 
+            return LocalDate.now().isAfter(dataLimiteDevolucao);
+        }
+        return dataDevolucao.isAfter(dataLimiteDevolucao); 
+    }
 
-   public Aluno getAluno() { return aluno; }
-   public Professor getProfessor() { return professor;}
-   public Bibliotecario getBibliotecario(){return bibliotecario;}
-   public Livro getLivro() { return livro; }
-   public LocalDate getDataEmprestimo() { return dataEmprestimo; }
-   public LocalDate getDataLimiteDevolucao() { return dataLimiteDevolucao; }
-   public LocalDate getDataDevolucao() { return dataDevolucao; }
+    
+    public long getDiasAtraso() {
+        LocalDate dataReferencia = (dataDevolucao != null) ? dataDevolucao : LocalDate.now();
+        if (dataReferencia.isAfter(dataLimiteDevolucao)) {
+            return ChronoUnit.DAYS.between(dataLimiteDevolucao, dataReferencia);
+        }
+        return 0;
+    }
 
-   public void setAluno(Aluno aluno) { this.aluno = aluno; }
-   public void setProfessor(Professor professor){this.professor=professor;}
-   public void setBibliotecario(Bibliotecario bibliotecario){this.bibliotecario=bibliotecario;}
-   public void setLivro(Livro livro) { this.livro = livro; }
-   public void setDataEmprestimo(LocalDate dataEmprestimo) { this.dataEmprestimo = dataEmprestimo; }
-   public void setDataLimiteDevolucao(LocalDate dataLimiteDevolucao) { this.dataLimiteDevolucao = dataLimiteDevolucao; }
-   public void setDataDevolucao(LocalDate dataDevolucao) { this.dataDevolucao = dataDevolucao; }
+    
+    public double getMulta() {
+        return getDiasAtraso() * MULTA_POR_DIA;
+    }
 
+    
+    public String getMultaFormatada() {
+        return String.format("R$ %.2f", getMulta());
+    }
 
-   public boolean isAtrasado() {
-	  if (dataDevolucao == null) {
-		 return LocalDate.now().isAfter(dataLimiteDevolucao);
-	  }
-	  return dataDevolucao.isAfter(dataLimiteDevolucao);
-   }
+    
+    public boolean renovar() {
+        if (qtdRenovacoes < LIMITE_RENOVACOES && dataDevolucao == null) {
+            dataLimiteDevolucao = dataLimiteDevolucao.plusDays(10);
+            qtdRenovacoes++;
+            return true;
+        }
+        return false;
+    }
 
+    
+    public String getStatus() {
+        if (dataDevolucao != null) {
+            return "Devolvido";
+        } else if (isAtrasado()) {
+            return "Atrasado";
+        } else {
+            return "Em andamento";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Empréstimo{" +
+                "usuario=" + usuario.getNome() +
+                ", livro=" + livro.getTitulo() +
+                ", limite=" + dataLimiteDevolucao +
+                ", devolvido=" + dataDevolucao +
+                ", renovacoes=" + qtdRenovacoes +
+                ", multa=" + getMultaFormatada() +
+                ", status=" + getStatus() +
+                '}';
+    }
 }
